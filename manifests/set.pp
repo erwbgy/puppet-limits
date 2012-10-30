ce4042f2b733d34e035fb9a514f766458db82cc9
Keith Burdis <keith@burdis.org>
2012-10-30 17:52:18 +0000

    Make it clear that the title is the domain.

--- manifests/set.pp
+++ manifests/set.pp
@@ -3,17 +3,18 @@ define limits::set (
   $soft = undef,
   $hard = undef
 ) {
+  $domain = $title
   if $soft {
-    limits::entry { "${title}-soft-${item}":
-      domain => $title,
+    limits::entry { "${domain}-soft-${item}":
+      domain => $domain,
       type   => 'soft',
       item   => $item,
       value  => $soft,
     }
   }
   if $hard {
-    limits::entry { "${title}-soft-${item}":
-      domain => $title,
+    limits::entry { "${domain}-hard-${item}":
+      domain => $domain,
       type   => 'hard',
       item   => $item,
       value  => $hard,
